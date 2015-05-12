0578c794e7f50a9c82761be76497142cd684bc4d
Russell Jancewicz <russell.jancewicz@gmail.com>
2015-05-12 11:52:23 -0400

    client fixes for 2.6

--- client/__init__.py
+++ client/__init__.py
@@ -7,59 +7,132 @@ import Cookie
     TODO: ssl connection
 """
 
-class SecureXMLRPCTransport(xmlrpclib.SafeTransport):
+if sys.version_info >= (2, 6) and sys.version_info < (2, 7):
+    class SecureXMLRPCTransport(xmlrpclib.SafeTransport):
 
-    xmlrpc_cookie = None
+        xmlrpc_cookie = None
 
-    def single_request(self, host, handler, request_body, verbose=0):
-        # issue XML-RPC request
+        def request(self, host, handler, request_body, verbose=0):
+            # issue XML-RPC request
 
-        h = self.make_connection(host)
-        if verbose:
-            h.set_debuglevel(1)
+            h = self.make_connection(host)
+            if verbose:
+                h.set_debuglevel(1)
 
-        try:
             self.send_request(h, handler, request_body)
             self.send_host(h, host)
             self.send_user_agent(h)
             self.send_cookie(h)
             self.send_content(h, request_body)
 
-            response = h.getresponse(buffering=True)
-            if response.status == 200:
-                self.verbose = verbose
-                return self.parse_response(response)
-        except Fault:
-            raise
-        except Exception:
-            # All unexpected errors leave connection in
-            # a strange state, so we clear it.
-            self.close()
-            raise
+            errcode, errmsg, headers = h.getreply()
 
-        #discard any response data and raise exception
-        if (response.getheader("content-length", 0)):
-            response.read()
-        raise ProtocolError(
-            host + handler,
-            response.status, response.reason,
-            response.msg,
-            )
+            if errcode != 200:
+                raise ProtocolError(
+                    host + handler,
+                    errcode, errmsg,
+                    headers
+                    )
 
-    def parse_response(self, response):
+            self.verbose = verbose
 
-        if hasattr(response,'getheader'):
-            cookie_str = response.getheader("Set-Cookie", None)
-            if cookie_str:
-                cookie = Cookie.SimpleCookie(cookie_str)
-                if cookie.has_key("XMLRPC_SESSION"):
-                    self.xmlrpc_cookie = cookie["XMLRPC_SESSION"].value
+            try:
+                sock = h._conn.sock
+            except AttributeError:
+                sock = None
 
-        return xmlrpclib.Transport.parse_response(self, response)
+            return self._parse_response(h.getfile(), sock)
 
-    def send_cookie(self, connection):
-        if self.xmlrpc_cookie:
-            connection.putheader("Cookie", "XMLRPC_SESSION=" + self.xmlrpc_cookie)
+        def _parse_response_headers(self, response):
+
+            if hasattr(response,'getheader'):
+                cookie_str = response.getheader("Set-Cookie", None)
+                if cookie_str:
+                    cookie = Cookie.SimpleCookie(cookie_str)
+                    if cookie.has_key("XMLRPC_SESSION"):
+                        self.xmlrpc_cookie = cookie["XMLRPC_SESSION"].value
+                        
+        def _parse_response(self, file, sock):
+            # read response from input file/socket, and parse it
+
+            p, u = self.getparser()
+
+            while 1:
+                if sock:
+                    response = sock.recv(1024)
+                else:
+                    response = file.read(1024)
+                if not response:
+                    break
+                if self.verbose:
+                    print "body:", repr(response)
+
+                self._parse_response_headers(response)
+                p.feed(response)
+
+            file.close()
+            p.close()
+
+            return u.close()
+
+        def send_cookie(self, connection):
+            if self.xmlrpc_cookie:
+                connection.putheader("Cookie", "XMLRPC_SESSION=" + self.xmlrpc_cookie)
+
+elif sys.version_info >= (2, 7) and sys.version_info < (3, 0):
+    class SecureXMLRPCTransport(xmlrpclib.SafeTransport):
+
+        xmlrpc_cookie = None
+
+        def single_request(self, host, handler, request_body, verbose=0):
+            # issue XML-RPC request
+
+            h = self.make_connection(host)
+            if verbose:
+                h.set_debuglevel(1)
+
+            try:
+                self.send_request(h, handler, request_body)
+                self.send_host(h, host)
+                self.send_user_agent(h)
+                self.send_cookie(h)
+                self.send_content(h, request_body)
+
+                response = h.getresponse(buffering=True)
+                if response.status == 200:
+                    self.verbose = verbose
+                    return self.parse_response(response)
+            except Fault:
+                raise
+            except Exception:
+                # All unexpected errors leave connection in
+                # a strange state, so we clear it.
+                self.close()
+                raise
+
+            #discard any response data and raise exception
+            if (response.getheader("content-length", 0)):
+                response.read()
+            raise ProtocolError(
+                host + handler,
+                response.status, response.reason,
+                response.msg,
+                )
+
+        def parse_response(self, response):
+
+            if hasattr(response,'getheader'):
+                cookie_str = response.getheader("Set-Cookie", None)
+                if cookie_str:
+                    cookie = Cookie.SimpleCookie(cookie_str)
+                    if cookie.has_key("XMLRPC_SESSION"):
+                        self.xmlrpc_cookie = cookie["XMLRPC_SESSION"].value
+
+            return xmlrpclib.Transport.parse_response(self, response)
+
+        def send_cookie(self, connection):
+            if self.xmlrpc_cookie:
+                connection.putheader("Cookie", "XMLRPC_SESSION=" + self.xmlrpc_cookie)
 
 class SecureXMLRPCClient(xmlrpclib.ServerProxy):
 
